83d7399900a87db282f732d2523e463fc315030f
YongwooKim <keepingitflow@gmail.com>
2022-02-23 21:31:51 +0900

    docs : Movie 데이터 구현

--- NowPlayingList/NowPlayingList/Model/Movie.swift
+++ NowPlayingList/NowPlayingList/Model/Movie.swift
@@ -5,16 +5,15 @@
 //  Created by Yongwoo Marco on 2022/02/17.
 //
 
-struct Movie: Decodable {
+struct Movie: Identifiable, Decodable {
     let id: Int
     let title: String
-    let releaseDate: String
-    let imagePath: String
+    let releaseDate: String?
+    let posterPath: String
     let rated: Double
     
     enum CodingKeys: String, CodingKey {
-        case id, title, releaseDate
-        case imagePath = "posterPath"
+        case id, title, releaseDate, posterPath
         case rated = "voteAverage"
     }
 }
